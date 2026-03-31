42ec20ae26b7cecccd81747c23d8d1214e698b5f
AbdirahmanWarsame <abdir.w99@gmail.com>
2026-03-31 16:00:09 +0200

    Add basic structure for movie watchlist

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h1>Movie Watchlist</h1>
+      <form>
+        <input type="text" placeholder="Movie title" />
+        <button type="submit">Add movie</button>
+      </form>
+
+      <h2>My Movies</h2>
+
+      <p>Harry Potter</p>
+    </div>
+  );
+}
+
+export default App;
