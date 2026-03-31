17631319c9590b6e4e7131991ebecde837f413a4
AbdirahmanWarsame <abdir.w99@gmail.com>
2026-03-31 18:20:08 +0200

    Add Navbar component to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
+import Navbar from "../components/Navbar";
+
 function HomePage() {
   return (
     <div>
+      <Navbar />
       <h1>Movie Watchlist</h1>
       <p>Keep track of movies you want to watch.</p>
     </div>
